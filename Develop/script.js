cf6de91df9d54b230eedd97db0b7e944efca0e15
CJ <ruachobeach@gmail.com>
2020-03-20 17:35:12 -0700

    final hw3

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
 
@@ -14,48 +13,44 @@ function writePassword() {
   passwordText.value = password;
 
 }
+var passWord = "";
 function generatePassword() {
-  var passWordLenght = prompt("Please Choose a Password length from 8-128?")
-  var passWordLowerCase = confirm("Is It Okay to put Lowercases in your Password?")
-  var passWordUpperCase = confirm("Is It Okay to put Uppercases in your Password?")
-  var passWordNum = confirm("Is it okay to have  numeric Values?")
-  var passWordSpecialCharacters = confirm("Is It Okay to put special characters in your Password?")
+  var passWordLn = prompt("Please Choose a Password length from 8-128?");
+  var passWordLn = parseInt(passWordLn);
+  if (typeof passWordLn === "number" && passWordLn > 8 && passWordLn < 128) {
+    var passWordLowerCase = confirm("Is It Okay to put Lowercases in your Password?");
+    var passWordUpperCase = confirm("Is It Okay to put Uppercases in your Password?");
+    var passWordNum = confirm("Is it okay to have  numeric Values?");
+    var passWordSpecialCharacters = confirm("Is It Okay to put special characters in your Password?");
 
-  var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  var passWordSpecialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"]
-  var passWord = ""
-  if (passWordLenght > 8 && passWordLenght < 128) {
-    if (passWordLowerCase && passWordUpperCase && passWordNum && passWordSpecialCharacters) {
-      for (i = 0; i < passWordLenght; i++) {
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)];
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)].toUpperCase();
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)]
-        passWord = passWord + [Math.floor(Math.random() * 9)];
-        passWord = passWord + passWordSpecialCharacters[Math.floor(Math.random() * passWordSpecialCharacters.length)];
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var passWordSpecialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+    passWord = "";
+    if (passWordLn > 8 && passWordLn < 128) {
+      while (passWord.length < passWordLn) {
+        if (passWordLowerCase && passWord.length < passWordLn) {
+          passWord = passWord + letters[Math.floor(Math.random() * letters.length)];
+        }
+        if (passWordUpperCase && passWord.length < passWordLn) {
+          passWord = passWord + letters[Math.floor(Math.random() * letters.length)].toUpperCase();
+        }
+        if (passWordNum && passWord.length < passWordLn) {
+          passWord = passWord + Math.floor(Math.random() * 9);
+        }
+        if (passWordSpecialCharacters === true && passWord.length < passWordLn) {
+          passWord = passWord + passWordSpecialChar[Math.floor(Math.random() * passWordSpecialChar.length)];
+        }
       }
     }
-    if (passWordLowerCase && passWordUpperCase && passWordNum) {
-      for (i = 0; i < passWordLenght; i++) {
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)];
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)].toUpperCase();
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)];
-        passWord = passWord + [Math.floor(Math.random() * 9)];
-      }
-    }
-    if (passWordLowerCase && passWordUpperCase) {
-      for (i = 0; i < passWordLenght; i++) {
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)];
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)].toUpperCase();
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)];
-      }
-    }
-    if (passWordLowerCase) {
-      for (i = 0; i < passWordLenght; i++) {
-        passWord = passWord + letters[Math.floor(Math.random() * letters.length)];
-      }
-    }
+    console.log(passWord.length);
+    console.log(passWordLowerCase + " Lower ");
+    console.log(passWordUpperCase + " Upper ");
+    console.log(passWordNum + " Num ");
+    console.log(passWordSpecialCharacters + " Char ");
+    return passWord;
+  }else {
+    alert("Follow directions");
+    generatePassword();
   }
-  console.log(passWord)
   return passWord;
-
 }
